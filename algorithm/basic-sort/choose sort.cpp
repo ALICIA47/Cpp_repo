f24f11ba20dd618820f9919cd57aa5e1dd267e3f
ALICIA47 <gnehiaboul@163.com>
2025-11-27 09:31:02 +0800

    finish commit

---
+++ algorithm/basic-sort/choose sort.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<sstream>
+#include<windows.h>
+#include<conio.h>
+using namespace std;
+class solution{
+public:
+    vector<int>choosesort(vector<int>& num){
+        int n=num.size();
+        for(int i=0;i<n;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++){
+                if(num[j]<num[min])min=j;
+            }
+            if(min!=i){
+                int temp=num[i];
+                num[i]=num[min];
+                num[min]=temp;
+            }
+        }
+        return num;
+    }
+};
+
+int main(){
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8); 
+    cout<<"请输入一系列整数，以空格间隔回车结束：\n";
+    string line;
+    getline(cin,line);
+    istringstream iss(line);
+    vector<int> num;
+    int x;
+    while(iss>>x) num.push_back(x);
+    cout<<"输入为：";
+    int n=num.size();
+    for(int i=0;i<n;i++){
+        cout<<num[i]<<" ";
+    }
+    cout<<endl;
+    solution sol;
+    vector<int>res=sol.choosesort(num);
+    cout<<"排序为：";
+    int r=res.size();
+    for(int i=0;i<r;i++){
+        cout<<res[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"按 Enter 退出...";
+    cin.get();
+    return 0;
+}
