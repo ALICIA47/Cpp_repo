2f6ec62891c67169e3730f2560527aa7c87f5a5d
ALICIA47 <gnehiaboul@163.com>
2025-12-14 07:47:05 +0800

    leetcode commit

---
+++ leetcode/leetcode 118.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        vector<vector<int>> generate(int numRows) {
+            vector<vector<int>> result;
+            if (numRows <= 0) return result;
+    
+            result.push_back({1}); 
+    
+            for (int i = 1; i < numRows; ++i) {
+                vector<int> current(i + 1, 1); 
+                for (int j = 1; j < i; ++j) {
+                    current[j] = result[i - 1][j - 1] + result[i - 1][j]; 
+                }
+                result.push_back(current); 
+            }
+    
+            return result;
+        }
+    };
